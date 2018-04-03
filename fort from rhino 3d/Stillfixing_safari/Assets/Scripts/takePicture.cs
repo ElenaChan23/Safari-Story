626dfd414941bfe05b985d19de1d3a5c8b4beda1
Miriam <36211784+mharries@users.noreply.github.com>
2018-04-03 17:00:23 -0400

    Backgrounds added and fade out working

---
+++ fort from rhino 3d/Stillfixing_safari/Assets/Scripts/takePicture.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class takePicture : MonoBehaviour {
+
+    public int imageWidth = 2048;
+    public bool saveAsJPEG = true;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            byte[] bytes = I360Render.Capture(imageWidth, saveAsJPEG);
+            if (bytes != null)
+            {
+                string path = Path.Combine(Application.persistentDataPath, "360render" + (saveAsJPEG ? ".jpeg" : ".png"));
+                File.WriteAllBytes(path, bytes);
+                Debug.Log("360 render saved to " + path);
+            }
+        }
+    }
+}
