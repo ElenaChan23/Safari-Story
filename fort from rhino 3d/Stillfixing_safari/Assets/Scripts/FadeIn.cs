a852c6f0b4f7b8fb9b33069e5ce0bbc0caaf3d54
Miriam <36211784+mharries@users.noreply.github.com>
2018-04-17 18:42:48 -0400

    auto scene change and new tent and spotlight

---
+++ fort from rhino 3d/Stillfixing_safari/Assets/Scripts/FadeIn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace VRStandardAssets.Utils
+{
+
+    public class FadeIn : MonoBehaviour
+    {
+
+        public float fadeLength;
+        public bool fadeAudio;
+        public VRCameraFade cameraFade;
+
+        // Use this for initialization
+        void Start()
+        {
+            cameraFade.FadeIn(fadeLength, fadeAudio);
+        }
+
+    }
+}
