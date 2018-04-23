9d287c56953c572ddada7781c249ce14c665fc4e
Miriam <36211784+mharries@users.noreply.github.com>
2018-04-22 21:46:03 -0400

    intro and outro added, not tested

--- fort from rhino 3d/Stillfixing_safari/Assets/Scripts/sceneChange.cs
+++ fort from rhino 3d/Stillfixing_safari/Assets/Scripts/sceneChange.cs
@@ -3,40 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class sceneChange : MonoBehaviour {
-
-    public float timeToNewScene;
-    public string sceneToLoad;
-
-    private void Start()
+namespace VRStandardAssets.Utils
+{
+    public class sceneChange : MonoBehaviour
     {
-        startTransitionTimer();
-    }
 
-    void goToScene()
-    {
-        StartCoroutine(LoadAsyncScene());
-    }
+        public float timeToNewScene;
+        public string sceneToLoad;
 
-    public void startTransitionTimer()
-    {
-        Invoke("goToScene", timeToNewScene);
-    }
+        public float fadeLength;
+        public bool fadeAudioAtEnd;
+        public VRCameraFade vrCamera;
 
-    IEnumerator LoadAsyncScene()
-    {
-        // The Application loads the Scene in the background as the current Scene runs.
-        // This is particularly good for creating loading screens.
-        // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
-        // a sceneBuildIndex of 1 as shown in Build Settings.
-
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
-
-        // Wait until the asynchronous scene fully loads
-        while (!asyncLoad.isDone)
+        private void Start()
         {
-            yield return null;
+            startTransitionTimer();
         }
-    }
 
+        void goToScene()
+        {
+            StartCoroutine(LoadAsyncScene());
+            Fade(fadeLength, fadeAudioAtEnd);
+        }
+
+        public void startTransitionTimer()
+        {
+            Invoke("goToScene", timeToNewScene);
+        }
+
+        IEnumerator LoadAsyncScene()
+        {
+            // The Application loads the Scene in the background as the current Scene runs.
+            // This is particularly good for creating loading screens.
+            // You could also load the Scene by using sceneBuildIndex. In this case Scene2 has
+            // a sceneBuildIndex of 1 as shown in Build Settings.
+
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
+
+            // Wait until the asynchronous scene fully loads
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+        }
+
+        public void Fade(float length, bool fadeAudio)
+        {
+            vrCamera.FadeOut(length, fadeAudio);
+        }
+
+    }
 }
